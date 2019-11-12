2864b6c51055b95efe2b2d8f0cc770f80d996b57
AbkKumar <AbkKumar@INHYDLABKKUMAR.logisticsdev.intra>
2019-11-12 17:28:01 +0530

    Added the Test Base Method.

--- SeleniumCodingChallange1/src/main/java/com/abk/base/TestBase.java
+++ SeleniumCodingChallange1/src/main/java/com/abk/base/TestBase.java
@@ -6,9 +6,14 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class TestBase {
 	
 	public static Properties prop;
+	public static WebDriver driver;
 	
 	public TestBase(){
 		
@@ -26,6 +31,69 @@ public class TestBase {
 			
 		}
 		
+	}
+	public static void initialization() {
+		
+		String browser = prop.getProperty("browser");
+		
+	}
+	
+	public static void getPath(String browser) {
+		
+		String os = System.getProperty("os.name");
+		
+		String driverPath = null;
+		
+		if(os.equalsIgnoreCase("windows")) {
+			
+			if(browser.equalsIgnoreCase("chrome") || browser.equalsIgnoreCase("google chrome")) {
+				
+				driverPath = ".//src//main//java//com//abk//resources//chromedriver.exe";
+				System.setProperty("webdriver.chrome.driver", driverPath);
+				driver = new ChromeDriver();
+				
+			}
+			else if(browser.equalsIgnoreCase("firefox") || browser.equalsIgnoreCase("mozila")) {
+				
+				driverPath = ".//src//main//java//com//abk//resources//geckodriver.exe";
+				System.setProperty("webdriver.gecko.driver", driverPath);
+				driver = new FirefoxDriver();
+				
+			}
+			else if(browser.equalsIgnoreCase("ie") || browser.equalsIgnoreCase("internet explorer")) {
+				
+				
+			}
+			
+		}
+		else if(os.equalsIgnoreCase("mac")) {
+			
+			if(browser.equalsIgnoreCase("chrome") || browser.equalsIgnoreCase("google chrome")) {
+				
+				driverPath = ".//src//main//java//com//abk//resources//chromedriver";
+				System.setProperty("webdriver.chrome.driver", driverPath);
+				driver = new ChromeDriver();
+				
+			}
+			else if(browser.equalsIgnoreCase("firefox") || browser.equalsIgnoreCase("mozila")) {
+				
+				driverPath = ".//src//main//java//com//abk//resources//geckodriver";
+				System.setProperty("webdriver.gecko.driver", driverPath);
+				driver = new FirefoxDriver();
+				
+			}
+			else if(browser.equalsIgnoreCase("ie") || browser.equalsIgnoreCase("internet explorer")) {
+				
+				
+			}
+			
+			
+		}
+		else if(os.equalsIgnoreCase("linux")) {
+			
+			
+			
+		}
 	}
 
 }
