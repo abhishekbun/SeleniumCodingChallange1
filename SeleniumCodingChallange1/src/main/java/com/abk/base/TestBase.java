668ad819f33183317c37b2ae6f84abc02cfd98cc
AbkKumar <AbkKumar@INHYDLABKKUMAR.logisticsdev.intra>
2019-11-13 17:49:50 +0530

    Updated the TestBase script.

--- SeleniumCodingChallange1/src/main/java/com/abk/base/TestBase.java
+++ SeleniumCodingChallange1/src/main/java/com/abk/base/TestBase.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+//import org.abk.crm.base.TestBase;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,13 +17,15 @@ public class TestBase {
 	public static WebDriver driver;
 	
 	public TestBase(){
-		
+		System.out.println("HI");
 		try {
 			prop = new Properties();
-			
-			FileInputStream fis = new FileInputStream(new File(System.getProperty("user.dir")+"/src/main/java/org/abk/crm/config/config.properties"));
+			File file = new File(System.getProperty("user.dir")+"/src/main/java/com/abk/config/config.properties");
+			FileInputStream fis = new FileInputStream(file);
 			
 			prop.load(fis);
+			System.out.println("Hello");
+			
 		}
 		catch(FileNotFoundException e1) {
 			
@@ -36,53 +39,57 @@ public class TestBase {
 		
 		String browser = prop.getProperty("browser");
 		
+		getPath(browser);
+		driver.get(prop.getProperty("executionEnvironment"));
+		driver.manage().window().maximize();
+		
 	}
 	
 	public static void getPath(String browser) {
 		
 		String os = System.getProperty("os.name");
-		
+		System.out.println("Operating system type is" + os);
 		String driverPath = null;
 		
-		if(os.equalsIgnoreCase("windows")) {
+		if(os.toUpperCase().contains("WINDOWS")) {
 			
-			if(browser.equalsIgnoreCase("chrome") || browser.equalsIgnoreCase("google chrome")) {
+			if(browser.toUpperCase().contains("CHROME") || browser.toUpperCase().contains("GOOGLE CHROME")) {
 				
 				driverPath = ".//src//main//java//com//abk//resources//chromedriver.exe";
 				System.setProperty("webdriver.chrome.driver", driverPath);
 				driver = new ChromeDriver();
 				
 			}
-			else if(browser.equalsIgnoreCase("firefox") || browser.equalsIgnoreCase("mozila")) {
+			else if(browser.toUpperCase().contains("FIREFOX") || browser.toUpperCase().contains("MOZILA")) {
 				
 				driverPath = ".//src//main//java//com//abk//resources//geckodriver.exe";
 				System.setProperty("webdriver.gecko.driver", driverPath);
 				driver = new FirefoxDriver();
 				
 			}
-			else if(browser.equalsIgnoreCase("ie") || browser.equalsIgnoreCase("internet explorer")) {
+			else if(browser.toUpperCase().contains("IE") || browser.toUpperCase().contains("INTERNET EXPLORER")) {
 				
 				
 			}
 			
 		}
-		else if(os.equalsIgnoreCase("mac")) {
+		else if(os.toUpperCase().equalsIgnoreCase("MAC")) {
 			
-			if(browser.equalsIgnoreCase("chrome") || browser.equalsIgnoreCase("google chrome")) {
+			if(browser.toUpperCase().contains("CHROME") || browser.toUpperCase().contains("GOOGLE CHROME")) {
 				
 				driverPath = ".//src//main//java//com//abk//resources//chromedriver";
 				System.setProperty("webdriver.chrome.driver", driverPath);
 				driver = new ChromeDriver();
 				
 			}
-			else if(browser.equalsIgnoreCase("firefox") || browser.equalsIgnoreCase("mozila")) {
+			else if(browser.toUpperCase().contains("FIREFOX") || browser.toUpperCase().contains("MOZILA")) {
 				
 				driverPath = ".//src//main//java//com//abk//resources//geckodriver";
 				System.setProperty("webdriver.gecko.driver", driverPath);
 				driver = new FirefoxDriver();
 				
 			}
-			else if(browser.equalsIgnoreCase("ie") || browser.equalsIgnoreCase("internet explorer")) {
+			else if(browser.toUpperCase().contains("IE") || browser.toUpperCase().contains("INTERNET EXPLORER")) {
 				
 				
 			}
@@ -95,5 +102,9 @@ public class TestBase {
 			
 		}
 	}
-
+	/*public static void main(String[] args) {
+		TestBase t1 = new TestBase();
+		initialization();
+	}
+*/
 }
